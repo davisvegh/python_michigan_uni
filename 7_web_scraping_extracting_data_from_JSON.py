9ab8b08f1a0ac6d93b5afededdf42656a8546235
David V <44787708+davisvegh@users.noreply.github.com>
2023-10-15 22:44:16 +0200

    Update and rename web_scraping_extracting_data_from_JSON.py to 7_web_scraping_extracting_data_from_JSON.py

--- 7_web_scraping_extracting_data_from_JSON.py
+++ 7_web_scraping_extracting_data_from_JSON.py
@@ -2,19 +2,24 @@ import json
 import urllib.parse, urllib.error, urllib.request
 import ssl
 
-#Ignore SSL certificate errors
+# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# Enter the URL from where the data will be processed
 url = input("Enter location: ")
+if (len(url) < 1): url = 'http://py4e-data.dr-chuck.net/comments_1891875.json'
 print("Retrieving ", url)
 
+# Retrieve data from the URL
 data = urllib.request.urlopen(url, context=ctx).read()
 
+# Load it into JSON and check the length
 jsondata = json.loads(data)
 print("Retrieved", len(data), "characters")
 
+# Grab a specific value from the JSON data
 comments = jsondata["comments"]
 sum = 0
 
