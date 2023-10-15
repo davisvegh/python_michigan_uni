74f8150ac1423b9b8d62b00b9b969cd1097b3eca
David V <44787708+davisvegh@users.noreply.github.com>
2023-10-15 22:50:05 +0200

    Update and rename web_scraping_calling_a_json_api.py to 8_web_scraping_calling_a_json_api_GEO.py

--- 8_web_scraping_calling_a_json_api_GEO.py
+++ 8_web_scraping_calling_a_json_api_GEO.py
@@ -4,6 +4,7 @@ import json
 
 api_key = False
 
+# If there is no specific API key for Google Maps, use Dr Chuck's data
 if api_key is False:
     api_key = 42
     serviceurl = "http://py4e-data.dr-chuck.net/json?"
@@ -16,18 +17,22 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 while True:
-
+    
+    # Enter a real city or place like 'Barcelona'
     address = input("Enter location: ")
 
+    # Get the parameters, the address and the API key, if there is, and insert it into the URL
     parms = dict()
     parms["address"] = address
     parms["key"] = api_key
     url = serviceurl + urllib.parse.urlencode(parms)
 
     print("Retrieving", url)
-
+    
+    # Open the URL
     uh = urllib.request.urlopen(url, context=ctx)
-
+    
+    # Decode the data to be able to process it
     data = uh.read().decode()
 
     print("Retrieved", len(data), "characters")
@@ -41,9 +46,9 @@ while True:
         print("==== Failure to retrieve ====")
         print(data)
         continue
-
+        
+    # Get the place_id for the entered site or location
     place_id = js["results"][0]["place_id"]
 
-    #print(json.dumps(js, indent=2))
     print("Place id", place_id)
 
